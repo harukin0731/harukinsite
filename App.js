86d2a5f889e2488119dff1ed882a7627adf3c501
harukin-DeskMini <harukin@haruk.in>
2021-12-01 04:19:09 +0900

    高速化テスト

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { AppLoading} from 'expo';
 import { useFonts } from '@use-expo/font';
 import { StyleSheet, Text, View } from 'react-native';
@@ -7,36 +7,45 @@ import { createStackNavigator,TransitionPresets } from '@react-navigation/stack'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { TopPage,  TopPageAbout, TopPageStatus } from './TopPage';
-import { AboutMe } from './Page/Aboutme';
 import { Page404 } from './404';
 import { navigationRef } from './RootNavigation.js';
 import { data as page2020102101 } from './Page/Info/2020_10_21_01';
 import * as Linking from "expo-linking";
 import * as RootNavigation from './RootNavigation.js';
-import JRShikoku from './Page/Apps/JRShikoku';
-import Apps from './Page/Apps';
-import Web from './Page/Web';
-import fm from './Page/Apps/fm';
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
 const Drawer = createDrawerNavigator();
 export default function App() {
-  test();
   let [fontsLoaded] = useFonts({
     'Inter-Black': require('./assets/font_1_ant-maru.ttf'),
   });
+
+  const [jrshikokuPage,setJrShikokuPage] = useState();
+  import('./Page/Apps/JRShikoku').then(d=>setJrShikokuPage(d));
+
+  const [appsPage,setAppsPage] = useState();
+  const [webPage,setWebPage] = useState();
+  const [fmPage,setFmPage] = useState();
+  useEffect(()=>{
+    Promise.allSettled([import('./Page/Apps').then(d=>setAppsPage(d)),import('./Page/Web').then(d=>setWebPage(d)),import('./Page/Apps/fm').then(d=>setFmPage(d))]).then((value)=>{
+      console.log(value)
+      
+    })  
+  },[])
+  
+  appsPage && webPage && fmPage && test()
+
   return (
     <NavigationContainer ref={navigationRef}>
       <Stack.Navigator headerMode="screen">
         <Stack.Screen name='トップページ' component={TopPage}       options={{headerShown: false}} path="Top"/>
         <Stack.Screen name='About'     component={TopPageAbout}  options={{...TransitionPresets.ModalSlideFromBottomIOS ,headerTitleAlign:"center",headerShown: true, animationEnabled: true }}/>
         <Stack.Screen name='Status'     component={TopPageStatus}  options={{...TransitionPresets.ModalSlideFromBottomIOS ,headerTitleAlign:"center",headerShown: true, animationEnabled: true }}/>
-        <Stack.Screen name='自分'         component={AboutMe}       options={{...TransitionPresets.SlideFromRightIOS ,headerTitleAlign:"center",headerShown: false, animationEnabled: true }}/>
         <Stack.Screen name='404'          component={Page404}       options={{...TransitionPresets.ModalSlideFromBottomIOS ,headerTitleAlign:"center",headerShown: false, animationEnabled: true }}/>
-        <Stack.Screen name='Web'          component={Web}           options={{...TransitionPresets.SlideFromRightIOS ,headerTitleAlign:"center",headerShown: false, animationEnabled: false }}/>
-        <Stack.Screen name='Apps'         component={Apps}          options={{...TransitionPresets.SlideFromRightIOS ,headerTitleAlign:"center",headerShown: false, animationEnabled: false }}/>
-        <Stack.Screen name='JRShikoku'    component={JRShikoku}     options={{...TransitionPresets.SlideFromRightIOS ,headerTitleAlign:"center",headerShown: false, animationEnabled: false }} path="JRShikoku"/>
-        <Stack.Screen name='fm'           component={fm}            options={{...TransitionPresets.SlideFromRightIOS ,headerTitleAlign:"center",headerShown: false, animationEnabled: false }}/>
+        {webPage        && <Stack.Screen name='Web'          component={webPage.default}           options={{...TransitionPresets.SlideFromRightIOS ,headerTitleAlign:"center",headerShown: false, animationEnabled: false }}/>}
+        {appsPage       && <Stack.Screen name='Apps'         component={appsPage.default}          options={{...TransitionPresets.SlideFromRightIOS ,headerTitleAlign:"center",headerShown: false, animationEnabled: false }}/>}
+        {jrshikokuPage  && <Stack.Screen name='JRShikoku'    component={jrshikokuPage.default}     options={{...TransitionPresets.SlideFromRightIOS ,headerTitleAlign:"center",headerShown: false, animationEnabled: false }} path="JRShikoku"/>}
+        {fmPage         && <Stack.Screen name='fm'           component={fmPage.default}            options={{...TransitionPresets.SlideFromRightIOS ,headerTitleAlign:"center",headerShown: false, animationEnabled: false }}/>}
       </Stack.Navigator>
     </NavigationContainer>
   );
