ea86e662357e7ae9f86349e3e45b808ab0e06c7f
harukin-DeskMini <harukin@haruk.in>
2021-12-09 03:53:43 +0900

    PWA対応化

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,16 @@
 const createExpoWebpackConfigAsync = require('@expo/webpack-config');
 
 module.exports = async function (env, argv) {
-  const config = await createExpoWebpackConfigAsync(env, argv);
+  //const config = await createExpoWebpackConfigAsync(env, argv);
+  // 上流から受け取った env に offline:true のキーバリューセットを追加している　要は offline 使うよ、って追加しているだけ
+  const config = await createExpoWebpackConfigAsync(
+    {
+        ...env,
+        offline: true,
+    },
+    argv
+);
+///////////////////////////////ここまで
   // Customize the config before returning it.
   return config;
 };
