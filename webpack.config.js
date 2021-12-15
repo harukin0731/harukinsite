bd28d761b4da6e01f15aedbb43f87172c53282b5
harukin-DeskMini <harukin@haruk.in>
2021-12-15 20:18:00 +0900

    Revert "PWA対応化"
    This reverts commit ea86e662357e7ae9f86349e3e45b808ab0e06c7f.

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,7 @@
 const createExpoWebpackConfigAsync = require('@expo/webpack-config');
 
 module.exports = async function (env, argv) {
-  //const config = await createExpoWebpackConfigAsync(env, argv);
-  // 上流から受け取った env に offline:true のキーバリューセットを追加している　要は offline 使うよ、って追加しているだけ
-  const config = await createExpoWebpackConfigAsync(
-    {
-        ...env,
-        offline: true,
-    },
-    argv
-);
-///////////////////////////////ここまで
+  const config = await createExpoWebpackConfigAsync(env, argv);
   // Customize the config before returning it.
   return config;
 };
